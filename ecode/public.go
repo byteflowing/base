e1ad5da09070e3484e5c5237b5266e2c8f0309f3
only <richard@yanrongliang.com>
2025-09-11 16:35:09 +0800

    feat: 调整错误代码

--- ecode/public.go
+++ ecode/public.go
@@ -8,6 +8,8 @@ import (
 var (
 	ErrInternal         = status.New(codes.Internal, "ERR_INTERNAL").Err()                   // 内部错误
 	ErrNotFound         = status.New(codes.NotFound, "ERR_NOT_FOUND").Err()                  // 资源不存在
+	ErrPhoneNotExist    = status.New(codes.NotFound, "ERR_PHONE_NOT_EXIST").Err()            // 手机号码不存在
+	ErrEmailNotExist    = status.New(codes.NotFound, "ERR_EMAIL_NOT_EXIST").Err()            // 邮箱不存在
 	ErrUnauthenticated  = status.New(codes.Unauthenticated, "ERR_UNAUTHORIZED").Err()        // 未认证
 	ErrPermission       = status.New(codes.PermissionDenied, "ERR_PERMISSION").Err()         // 无权限
 	ErrTimeout          = status.New(codes.DeadlineExceeded, "ERR_TIMEOUT").Err()            // 超时
@@ -18,10 +20,8 @@ var (
 	ErrEmailNotMatch    = status.New(codes.InvalidArgument, "ERR_EMAIL_NOT_MATCH").Err()     // 邮箱不匹配
 	ErrPhoneIsEmpty     = status.New(codes.InvalidArgument, "ERR_PHONE_IS_EMPTY").Err()      // 手机号码为空
 	ErrEmailIsEmpty     = status.New(codes.InvalidArgument, "ERR_EMAIL_IS_EMPTY").Err()      // 邮箱为空
-	ErrPhoneNotExist    = status.New(codes.InvalidArgument, "ERR_PHONE_NOT_EXIST").Err()     // 手机号码不存在
-	ErrEmailNotExist    = status.New(codes.InvalidArgument, "ERR_EMAIL_NOT_EXIST").Err()     // 邮箱不存在
-	ErrPhoneExists      = status.New(codes.InvalidArgument, "ERR_PHONE_EXISTS").Err()        // 手机已存在
-	ErrEmailExists      = status.New(codes.InvalidArgument, "ERR_EMAIL_EXISTS").Err()        // 邮箱已存在
+	ErrPhoneExists      = status.New(codes.AlreadyExists, "ERR_PHONE_EXISTS").Err()          // 手机已存在
+	ErrEmailExists      = status.New(codes.AlreadyExists, "ERR_EMAIL_EXISTS").Err()          // 邮箱已存在
 	ErrEmailAlreadyBind = status.New(codes.AlreadyExists, "ERR_EMAIL_ALREADY_BIND").Err()    // 邮箱已绑定
 	ErrPhoneAlreadyBind = status.New(codes.AlreadyExists, "ERR_PHONE_ALREADY_BIND").Err()    // 手机已绑定
 )
